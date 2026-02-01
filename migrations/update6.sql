cd559888a9e9ca49ee2279cba158dc161deaa3e3
Neelima Mustak Richi <neelimaaaaaa@gmail.com>
2026-02-01 17:08:56 +0600

    Modification

--- migrations/update6.sql
+++ migrations/update6.sql
@@ -2,5 +2,5 @@ ALTER TABLE Vendors
 ADD COLUMN whatsapp_link VARCHAR(500) null,
 ADD COLUMN service_radius_km INT DEFAULT 5 not null,
 ADD COLUMN visiting_card_url VARCHAR(500),
-ADD COLUMN shop_address TEXT not null,
+ADD COLUMN shop_address TEXT null,
 ADD COLUMN service_locations JSON;
